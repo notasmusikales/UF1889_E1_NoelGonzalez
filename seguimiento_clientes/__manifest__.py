e8130767fd0dbdde3f1fc11e5dcdb34c716298cb
noel <notasmusikales@gmail.com>
2026-04-21 10:43:01 +0100

    Add access control and restructure menu items in Seguimiento Clientes

--- seguimiento_clientes/__manifest__.py
+++ seguimiento_clientes/__manifest__.py
@@ -3,6 +3,7 @@
     'version': '1.0',
     'depends': ['base', 'contacts'],
     'data': [
+        'security/ir.model.access.csv',
         'views/seguimiento_views.xml',
     ],
     'installable': True,
